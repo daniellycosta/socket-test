fcdb1f24df3ae110cd89effd3dc2cd3f5224eb6d
Guilherme Matheus Borges de Oliveira <gmbo182@hotmail.com>
2017-08-07 10:12:24 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
-const handler = (req, res) => {
-  fs.readFile(__dirname + '/index.html', (err, data) => {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-    res.writeHead(200);
-    res.end(data);
-  });
-}
+const handler = (req, res) => { console.log(req.url) }
 
 const app = require('http').createServer(handler);
 const io = require('socket.io')(app);
